c5e0fe57f178dc41c5377ccb321f236f179e5c25
hayoungnemo <hylee2@sugarhill.co.kr>
2024-01-07 19:53:03 +0900

    force-dynamic

--- src/app/board/read/[id]/page.tsx
+++ src/app/board/read/[id]/page.tsx
@@ -1,30 +1,31 @@
-import UpdateDelete from "@/component/UpdateDelete";
+import UpdateDelete from '@/component/UpdateDelete';
+export const dynamic = 'force-dynamic';
 
-import Link from "next/link";
+import Link from 'next/link';
 export default async function Read(props: { params: { id: string } }) {
-  const id = props.params.id;
+	const id = props.params.id;
 
-  const resp = await fetch(`http://localhost:9999/posts/${id}`, {
-    cache: "no-cache",
-  });
-  const topic = await resp.json();
-  return (
-    <div className="board-list">
-      <div className="content-title">
-        <h2>제목: {topic.title}</h2>
-        <h3>{topic.author}</h3>
-      </div>
-      <div>
-        <p className="content">{topic.body}</p>
-      </div>
-      <div>
-        <div className="button">
-          <Link href="/board/read">리스트로 돌아가기</Link>
-        </div>
-        <div className="update-delete">
-          <UpdateDelete id={id} />
-        </div>
-      </div>
-    </div>
-  );
+	const resp = await fetch(`http://localhost:9999/posts/${id}`, {
+		cache: 'no-cache',
+	});
+	const topic = await resp.json();
+	return (
+		<div className='board-list'>
+			<div className='content-title'>
+				<h2>제목: {topic.title}</h2>
+				<h3>{topic.author}</h3>
+			</div>
+			<div>
+				<p className='content'>{topic.body}</p>
+			</div>
+			<div>
+				<div className='button'>
+					<Link href='/board/read'>리스트로 돌아가기</Link>
+				</div>
+				<div className='update-delete'>
+					<UpdateDelete id={id} />
+				</div>
+			</div>
+		</div>
+	);
 }
